a5d8d1658b533e23e18b66da95166899d3016d89
JuniorMello25 <115903774+JuniorMello25@users.noreply.github.com>
2023-01-05 10:57:50 -0300

    Add files via upload

---
+++ Conexao.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CamadaDados
+{
+    class Conexao
+    {
+        public static string Cn = "Data Source = DESKTOP-VD9DNQP; Initial Catalog = dbcomercio; Integrated Security=true";
+    }
+
+}
